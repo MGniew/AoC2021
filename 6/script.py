0df19e0488cf29feaae5fdc71f26bd99a39d47bb
MGniew <m.f.gniewkowski@gmail.com>
2021-12-06 18:03:59 +0100

    Day 6

---
+++ 6/script.py
@@ -0,0 +1,22 @@
+def solve(data, days):
+    data = data.copy()
+    for d in range(days):
+        for k in sorted(data.keys()):
+            data[k - 1] = data[k]
+        data[8] = data[-1]
+        data[6] += data[-1]
+        data.pop(-1)
+
+    print(sum(data.values()))
+
+def main():
+    with open("input") as f:
+        data = [int(el) for el in f.read().split(",")]
+    data = {el: data.count(el) for el in set(data)}
+    data = {k: (data[k] if k in data else 0) for k in range(9)}
+    solve(data, days=80)
+    solve(data, days=256)
+
+
+if __name__ == "__main__":
+    main()
